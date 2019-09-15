b15000481d5d31eef3165bdebfba62344b40fd7c
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2019-09-15 10:10:58 +0530

    Scraper 2.0
    - Complete Rework of the code
    - Dedicated functions relating to parsing, scraping and downloading
    - New arguments for downloading Pics for students in a particular hostel
      or interning in PS2 or Thesis

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 ## How to use
 
 ```
-	$ python scraper.py year
+	$ scraper.py [-h] [--hostel HOSTEL] [-i INTERN] year
 ```
 
-*	year = 2016, 2017 etc.
+*	year = Starting year of the Student (2016, 2017 etc.)
+*	hostel = Ids of Students residing in a particular hostel (AH7, CH5 etc.)
+*	intern = Students currently doing PS2 or Thesis
--- scraper.py
+++ scraper.py
@@ -7,18 +7,47 @@ import argparse
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-parser = argparse.ArgumentParser(description = "get the hostel")
-parser.add_argument('YEAR', help = "Enter Hostel")
-args = parser.parse_args()
-YEAR = args.YEAR
+def make_parser():
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawTextHelpFormatter)
+	
+	parser.add_argument('year',
+		type=str,
+		default='',
+		help='Starting year of the Student')
+	
+	parser.add_argument('--hostel',
+		type=str,
+		default=None,
+		help='Hostel which the students reside in')
+		
+	parser.add_argument('-i', '--intern',
+		type=str,
+		default=None,
+		help='Student currently doing PS2 or thesis')
+	
+	return parser
+
 
 def main():
+	args = make_parser().parse_args()
+	year = args.year
+	hostel = args.hostel 
+	
 	url = "https://swd.bits-goa.ac.in/search1.php"
 	r = requests.get(url, verify=False)
 	soup = bs(r.text, "html.parser")
-	ids = [tag.td.text.strip() for tag in soup.find_all('tr')]
+	
+	if args.intern == 'PS2' or args.intern == 'Thesis':
+		hostel = args.intern
+	
+	if hostel:
+		ids = [tag.td.text.strip() for tag in soup.find_all('tr') if hostel in str(tag)]
+	else:
+		ids = [tag.td.text.strip() for tag in soup.find_all('tr')]
+		
 	printer(ids)
-	download(ids)
+	download(ids, year)
 	
 def printer(data):
 	with open("ID.txt", 'w') as f:
@@ -27,17 +56,17 @@ def printer(data):
 				f.write(column)
 			f.write('\n')
 			
-def download(data):
+def download(data, year):
 	with open("ID.txt") as f:
 		content = f.readlines()
 	content = [x.strip() for x in content]
 	content = [x.replace("G","") for x in content]
 	for column in content:
-			if YEAR in column:
-				response = requests.get("https://swd.bits-goa.ac.in/css/studentImg/" + column + ".jpg", verify=False, stream = True)
-				with open(column + '.jpg', 'wb') as out_file:
-					shutil.copyfileobj(response.raw, out_file)
-				del response
+		if year in column:
+			response = requests.get("https://swd.bits-goa.ac.in/css/studentImg/" + column + ".jpg", verify=False, stream = True)
+			with open(column + '.jpg', 'wb') as out_file:
+				shutil.copyfileobj(response.raw, out_file)
+			del response
 		
 if __name__ == "__main__":
 	main()
